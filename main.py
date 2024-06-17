d5862a86dc20deb7918da692d2e6d7728340a040
carlostechinnovation <2873152+carlostechinnovation@users.noreply.github.com>
2024-06-17 10:53:01 +0200

    Se resuelven conflictos

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ from time import sleep
 def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
-<<<<<<< HEAD
-for idx, result in enumerate(roll(5,20)):
-=======
-for idx, result in enumerate(roll(6,6)):
->>>>>>> main
+
+for idx, result in enumerate(roll(6,20)):
     print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
 
